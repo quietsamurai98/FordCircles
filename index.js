9a4417ccfc500437f64ffd1a5e3ab94a9cb49251
quietsamurai98 <quietsamurai98@gmail.com>
2017-05-03 18:16:47 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var svgImg;
-
-
+var anim = 0;
+var colors = ['red', 'orangered', 'goldenrod', 'darkgreen', 'navy', 'indigo', 'violet'];
 function pageLoad(){
-    window.addEventListener("resize", resizeSVG);
+    //window.addEventListener("resize", resizeSVG);
     init();
 }
 function getElem(idStr){
@@ -13,52 +13,58 @@ function init(){
     
     //getElem("svgDiv").innerHTML = "<svg id='svgImg' height=" + window.innerHeight + " width=" + window.innerWidth + " viewbox = '-0.5 0 2 1'></svg>";
     svgImg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    //resizeSVG();
-    svgImg.setAttribute("preserveAspectRatio", "xMidYMin slice");
+    resizeSVG();
+    svgImg.setAttribute("width", window.innerWidth);
+    svgImg.setAttribute("height", window.innerHeight);
+    //svgImg.setAttribute("preserveAspectRatio", "xMidYMax slice");
     svgImg.setAttribute("viewBox", "-0.5 0 2 1");
-    //svgImg.setAttribute("style", "border-bottom: 1px solid black");
-    getElem("svgDiv").appendChild(svgImg);
-    // addFordCircle(0,1);
-    // addFordCircle(1,1);
-    // addFordCircle(1,2);
-    run(100);
+    svgImg.setAttribute("style", "margin: 0 !important; padding: 0 !important;");
+    document.body.appendChild(svgImg);
+    addFordCircle(0,1);
+    addFordCircle(1,1);
+    //svgImg.setAttribute("height", document.body.clientHeight);
+    svgImg.setAttribute("width", document.body.clientWidth);
+    play(100);
 }
 
 function resizeSVG(){
-    svgImg.setAttribute("width", window.innerWidth * 1);
-    svgImg.setAttribute("height", window.innerHeight * 0.8);
+    //svgImg.setAttribute("width", "100%");
+    //svgImg.setAttribute("height", "auto");
 }
 
 function run(n){
-    for(var a = 1; a <= n; a++){
+    while(svgImg.firstChild){
+        svgImg.removeChild(svgImg.firstChild);
+    }
+    addFordCircle(0,1);
+    addFordCircle(1,1);
+    for(var a = 2; a <= n; a++){
         for(var b = 0; b <= a; b++){
-            //if(1==gcd(b, a)){
+            if(1==gcd(b, a)){
                 addFordCircle(b,a);
-            //}
+            }
         }
     }
+    if(n>100){clearInterval(anim);}
+}
+
+function play(ms){
+    var iter = 1;
+    if(anim){
+        clearInterval(anim);
+    }
+    anim = setInterval(function(){ run(iter++) }, ms);
 }
 
 function gcd(a,b) {
-  a = Math.abs(a);
-  b = Math.abs(b);
- 
-  if (b > a) {
-    var temp = a;
-    a = b;
-    b = temp; 
-  }
- 
-  while (true) {
-    a %= b;
-    if (a == 0) { return b; }
-    b %= a;
-    if (b == 0) { return a; }
-  }
+    if (a%b === 0) { 
+        return b; 
+    } 
+    return gcd(b,a%b);
 }
 
 function addFordCircle(p, q){
-    addCircle(p/q, 1/(2*q*q), 1/(2*q*q), "black")
+    addCircle(p/q, 1/(2*q*q), 1/(2*q*q), colors[(q-1)%6])
 }
 function addCircle(x, y, r, c){
     y = 1.0 - y; //Flips cartesian coordinate to svg coordinate
