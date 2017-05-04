ea2b5ec6703428f43cff434b53e0b16774a01a5f
quietsamurai98 <quietsamurai98@gmail.com>
2017-05-04 09:51:12 -0500

    Added zoom and pan!
    WASD or arrow keys to pan
    Scroll wheel to zoom

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var svgImg;
-var anim = 0;
+var animDelay = 100;
+var iter;
+var MAX_DEPTH = 250;
 var colors = ['red', 'orangered', 'goldenrod', 'darkgreen', 'navy', 'indigo', 'violet'];
 function pageLoad(){
-    //window.addEventListener("resize", resizeSVG);
+    window.addEventListener('wheel', function(e) { zoom(e); });
+    window.addEventListener('keydown', function(e) { pan(e); });
     init();
 }
 function getElem(idStr){
@@ -10,50 +13,26 @@ function getElem(idStr){
 }
 
 function init(){
-    
-    //getElem("svgDiv").innerHTML = "<svg id='svgImg' height=" + window.innerHeight + " width=" + window.innerWidth + " viewbox = '-0.5 0 2 1'></svg>";
+    setupSVG();
+    iter = 0;
+    play();
+}
+
+function setupSVG(){
     svgImg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    resizeSVG();
-    svgImg.setAttribute("width", window.innerWidth);
-    svgImg.setAttribute("height", window.innerHeight);
-    //svgImg.setAttribute("preserveAspectRatio", "xMidYMax slice");
+    svgImg.setAttribute("width", document.body.clientWidth);
+    svgImg.setAttribute("height", document.body.clientWidth/2);
     svgImg.setAttribute("viewBox", "-0.5 0 2 1");
     svgImg.setAttribute("style", "margin: 0 !important; padding: 0 !important;");
     document.body.appendChild(svgImg);
-    addFordCircle(0,1);
-    addFordCircle(1,1);
-    //svgImg.setAttribute("height", document.body.clientHeight);
-    svgImg.setAttribute("width", document.body.clientWidth);
-    play(100);
 }
 
-function resizeSVG(){
-    //svgImg.setAttribute("width", "100%");
-    //svgImg.setAttribute("height", "auto");
-}
-
-function run(n){
-    while(svgImg.firstChild){
-        svgImg.removeChild(svgImg.firstChild);
+function play(){
+    iter++
+    addIteration(iter);
+    if(iter<MAX_DEPTH){
+        setTimeout(play, animDelay);
     }
-    addFordCircle(0,1);
-    addFordCircle(1,1);
-    for(var a = 2; a <= n; a++){
-        for(var b = 0; b <= a; b++){
-            if(1==gcd(b, a)){
-                addFordCircle(b,a);
-            }
-        }
-    }
-    if(n>100){clearInterval(anim);}
-}
-
-function play(ms){
-    var iter = 1;
-    if(anim){
-        clearInterval(anim);
-    }
-    anim = setInterval(function(){ run(iter++) }, ms);
 }
 
 function gcd(a,b) {
@@ -61,6 +40,14 @@ function gcd(a,b) {
         return b; 
     } 
     return gcd(b,a%b);
+}
+
+function addIteration(q){
+    for (var p=0; p<=q; p++){
+        if(gcd(p, q)==1){
+            addFordCircle(p, q);
+        }
+    }
 }
 
 function addFordCircle(p, q){
@@ -77,4 +64,62 @@ function addCircle(x, y, r, c){
     svgImg.appendChild(circle);
 }
 
-//function 
+function zoom(e){ 
+    if (e.deltaY !== 0) {
+        var zoomFactor = 3;
+        var initialViewBox = svgImg.getAttribute("viewBox");
+        var viewBoxArr = initialViewBox.split(" ").map(parseFloat);
+        var x = viewBoxArr[0];
+        var y = viewBoxArr[1];
+        var w = viewBoxArr[2];
+        var h = viewBoxArr[3];
+        var cx = x+(w/2);
+        var cy = y+(h/2);
+        if (e.deltaY < 0) { //scroll up = zoom in
+            w/=zoomFactor;
+            h/=zoomFactor;
+            x=cx-(w/2);
+            y=cy-(h/2);
+        } else { //scroll down = zoom out
+            w*=zoomFactor;
+            h*=zoomFactor;
+            x=cx-(w/2);
+            y=cy-(h/2);
+        }
+        console.log("Old viewBox: " + initialViewBox);
+        var outputViewBox = [x,y,w,h].join(" ");
+        svgImg.setAttribute("viewBox", outputViewBox);
+        console.log("New viewBox: " + outputViewBox);
+    }
+}
+
+function pan(e){
+    console.log(e.key);
+    if(e.key.substr(0,5) === "Arrow" || ["w","a","s","d","W","A","S","D"].indexOf(e.key)>-1){
+        var initialViewBox = svgImg.getAttribute("viewBox");
+        var viewBoxArr = initialViewBox.split(" ").map(parseFloat);
+        var x = viewBoxArr[0];
+        var y = viewBoxArr[1];
+        var w = viewBoxArr[2];
+        var h = viewBoxArr[3];
+        
+        if(e.key === "ArrowDown" || e.key.toLowerCase() === "s"){
+            console.log("D");
+            y+=w/100;
+        } else if(e.key === "ArrowUp" || e.key.toLowerCase() === "w"){
+            console.log("U");
+            y-=w/100;
+        } else if(e.key === "ArrowLeft" || e.key.toLowerCase() === "a"){
+            console.log("L");
+            x-=w/100;
+        } else if(e.key === "ArrowRight" || e.key.toLowerCase() === "d"){
+            console.log("R");
+            x+=w/100;
+        }
+        
+        console.log("Old viewBox: " + initialViewBox);
+        var outputViewBox = [x,y,w,h].join(" ");
+        svgImg.setAttribute("viewBox", outputViewBox);
+        console.log("New viewBox: " + outputViewBox);
+    }
+}
